911e51c3b8d8edd2899a9f5220b96875d3e70f58
Simon VB <simon.vanblerk@trustpower.com>
2018-12-18 10:37:52 +0000

    Remove button react native component as it is no longer used - replace with SoundButton component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import SoundButton from './components/soundbutton';
 
 export default class App extends React.Component {
